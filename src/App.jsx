a0c6ecff3511d01d4ccf5928617a234518d6cf1f
Bassem Kallel <kallelbassem@bizerte.r-iset.tn>
2025-07-13 14:59:15 +0100

    Initialisation

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Button } from "@material-tailwind/react";
+
+function App() {
+  
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import './index.css'
+import App from './App.jsx'
+import { ThemeProvider } from '@material-tailwind/react'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </BrowserRouter>
+)
